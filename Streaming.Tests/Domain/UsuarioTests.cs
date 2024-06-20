c709ec63201dae6029cc2b96581c36e878fca2db
GuilhermeDuques <guiduques@gmail.com>
2024-06-20 17:33:07 -0300

    add test

--- Streaming.Tests/Domain/UsuarioTests.cs
+++ Streaming.Tests/Domain/UsuarioTests.cs
@@ -4,8 +4,8 @@ using Streaming.Domain.Transaction;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xunit;
+using Streaming.Domain.Streaming;
 
 namespace Streaming.Tests.Domain
 {
@@ -34,8 +34,8 @@ namespace Streaming.Tests.Domain
             Assert.True(usuario.Cartoes.Count == 1);
             Assert.True(usuario.Assinaturas.Count == 1);
             Assert.True(usuario.Assinaturas.First().Plano.Nome == plano.Nome);
-
         }
+
         [Fact]
         public void NaoDeveCriarUsuarioCasoLimiteCartaoForMenorValorPlano()
         {
@@ -53,7 +53,6 @@ namespace Streaming.Tests.Domain
                 var usuario = new Usuario();
                 usuario.Criar("Lorem ipsum", plano, cartao);
             });
-
         }
 
         [Fact]
@@ -73,6 +72,94 @@ namespace Streaming.Tests.Domain
                 var usuario = new Usuario();
                 usuario.Criar("Lorem ipsum", plano, cartao);
             });
+        }
+
+        [Fact]
+        public void CriarPlaylist_DeveAdicionarNovaPlaylist()
+        {
+            // Arrange
+            var usuario = new Usuario();
+
+            // Act
+            usuario.CriarPlaylist("Minha Playlist", true);
+
+            // Assert
+            Assert.Single(usuario.Playlists);
+            var playlist = usuario.Playlists.First();
+            Assert.Equal("Minha Playlist", playlist.Nome);
+            Assert.True(playlist.Publica);
+            Assert.Equal(usuario, playlist.Usuario);
+        }
+
+        [Fact]
+        public void FavoritarMusica_DeveAdicionarMusicaNaPlaylist()
+        {
+            // Arrange
+            var usuario = new Usuario();
+            usuario.CriarPlaylist("Favoritas", false);
+            var musica = new Musica { Id = Guid.NewGuid(), Nome = "Musica 1" };
+
+            // Act
+            usuario.FavoritarMusica(musica);
+
+            // Assert
+            var playlist = usuario.Playlists.First(x => x.Nome == "Favoritas");
+            Assert.Single(playlist.Musicas);
+            Assert.Contains(musica, playlist.Musicas);
+        }
+
+        [Fact]
+        public void FavoritarMusica_DeveLancarExceptionSePlaylistNaoEncontrada()
+        {
+            // Arrange
+            var usuario = new Usuario();
+            var musica = new Musica { Id = Guid.NewGuid(), Nome = "Musica 1" };
+
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => usuario.FavoritarMusica(musica));
+            Assert.Equal("Não encontrei a playlist", exception.Message);
+        }
+
+        [Fact]
+        public void DesfavoritarMusica_DeveRemoverMusicaDaPlaylist()
+        {
+            // Arrange
+            var usuario = new Usuario();
+            usuario.CriarPlaylist("Favoritas", false);
+            var musica = new Musica { Id = Guid.NewGuid(), Nome = "Musica 1" };
+            usuario.FavoritarMusica(musica);
+
+            // Act
+            usuario.DesfavoritarMusica(musica);
+
+            // Assert
+            var playlist = usuario.Playlists.First(x => x.Nome == "Favoritas");
+            Assert.Empty(playlist.Musicas);
+        }
+
+        [Fact]
+        public void DesfavoritarMusica_DeveLancarExceptionSePlaylistNaoEncontrada()
+        {
+            // Arrange
+            var usuario = new Usuario();
+            var musica = new Musica { Id = Guid.NewGuid(), Nome = "Musica 1" };
+
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => usuario.DesfavoritarMusica(musica));
+            Assert.Equal("Não encontrei a playlist", exception.Message);
+        }
+
+        [Fact]
+        public void DesfavoritarMusica_DeveLancarExceptionSeMusicaNaoEncontrada()
+        {
+            // Arrange
+            var usuario = new Usuario();
+            usuario.CriarPlaylist("Favoritas", false);
+            var musica = new Musica { Id = Guid.NewGuid(), Nome = "Musica 1" };
+
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => usuario.DesfavoritarMusica(musica));
+            Assert.Equal("Não encontrei a musica", exception.Message);
         }
     }
 }
