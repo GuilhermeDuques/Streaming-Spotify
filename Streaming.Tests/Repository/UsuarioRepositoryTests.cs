7902288224763b4e74547b8a14e2293ed67fd291
GuilhermeDuques <guiduques@gmail.com>
2024-06-20 22:13:55 -0300

    hhg

---
+++ Streaming.Tests/Repository/UsuarioRepositoryTests.cs
@@ -0,0 +1,90 @@
+﻿using Streaming.Domain.Account;
+using Streaming.Repository.Account;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Streaming.Tests.Repository
+{
+    public class UsuarioRepositoryTests
+    {
+        [Fact]
+        public void DeveSalvarUsuarioComSucesso()
+        {
+            // Arrange
+            var repository = new UsuarioRepository();
+            var usuario = new Usuario { Nome = "Usuario Teste" };
+
+            // Act
+            repository.Save(usuario);
+
+            // Assert
+            var savedUsuario = repository.GetUsuario(usuario.Id);
+            Assert.NotNull(savedUsuario);
+            Assert.Equal("Usuario Teste", savedUsuario.Nome);
+        }
+
+        [Fact]
+        public void DeveObterUsuarioPorId()
+        {
+            // Arrange
+            var repository = new UsuarioRepository();
+            var usuario = new Usuario { Nome = "Usuario Teste" };
+            repository.Save(usuario);
+
+            // Act
+            var fetchedUsuario = repository.GetUsuario(usuario.Id);
+
+            // Assert
+            Assert.NotNull(fetchedUsuario);
+            Assert.Equal(usuario.Id, fetchedUsuario.Id);
+        }
+
+        [Fact]
+        public void DeveAtualizarUsuario()
+        {
+            // Arrange
+            var repository = new UsuarioRepository();
+            var usuario = new Usuario { Nome = "Usuario Teste" };
+            repository.Save(usuario);
+
+            // Act
+            usuario.Nome = "Usuario Atualizado";
+            repository.Update(usuario);
+            var updatedUsuario = repository.GetUsuario(usuario.Id);
+
+            // Assert
+            Assert.NotNull(updatedUsuario);
+            Assert.Equal("Usuario Atualizado", updatedUsuario.Nome);
+        }
+
+        [Fact]
+        public void DeveRemoverUsuario()
+        {
+            // Arrange
+            var repository = new UsuarioRepository();
+            var usuario = new Usuario { Nome = "Usuario Teste" };
+            repository.Save(usuario);
+
+            // Act
+            repository.Remove(usuario);
+            var removedUsuario = repository.GetUsuario(usuario.Id);
+
+            // Assert
+            Assert.Null(removedUsuario);
+        }
+
+        [Fact]
+        public void NaoDeveEncontrarUsuarioInexistente()
+        {
+            // Arrange
+            var repository = new UsuarioRepository();
+
+            // Act
+            var usuario = repository.GetUsuario(Guid.NewGuid());
+
+            // Assert
+            Assert.Null(usuario);
+        }
+    }
+}
