5b495f1feac962042be150aa6b391a8e939233bd
GuilhermeDuques <guiduques@gmail.com>
2024-06-23 04:40:06 -0300

    Application Insights

--- Streaming.API/Program.cs
+++ Streaming.API/Program.cs
@@ -33,6 +33,10 @@ builder.Services.AddScoped<IAzureServiceBusService, AzureServiceBusService>();
 
 
 builder.Services.AddScoped<UsuarioService>();
+builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
+{
+    ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+});
 
 var app = builder.Build();
 
